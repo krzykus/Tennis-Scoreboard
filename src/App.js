6746884ca0bbb9552f90f7eed0ac0bc029a786e2
Tomasz <krzykophil@gmail.com>
2020-05-29 04:52:08 +0100

    Init (not really)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+import Player from './components/Player';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOne: {currentScore: 0, newScore:0, name: "Roger", img:"/img/roger.png"},
+      playerTwo: {currentScore: 0, newScore:0, name: "Tim", img:"/img/tim.png"}
+    };
+  }
+
+  addScore = () => {//This probably can be done better but for 5am can do...
+    console.log(this.state);
+    let randomPlayer = Math.random();
+    let players = [];
+    players.push(randomPlayer < 0.5 ? this.state.playerOne : this.state.playerTwo);
+    players.push(randomPlayer < 0.5 ? this.state.playerTwo : this.state.playerOne);
+    players[0].currentScore = players[0].newScore
+    players[0].newScore++;
+    if(players[0].newScore==5 || (players[0].newScore==4 && players[1].newScore==2))
+    {
+      //TODO: Show alert that player won
+      if(randomPlayer<0.5)
+      {
+        //PlayerOne won
+      }
+      else
+      {
+        //PlayerTwo won
+      }
+      this.restart();
+    }
+    else if(players[0].newScore==4 && players[1].newScore==4)
+    {
+      players[1].currentScore = 4;
+      players[1].newScore = 3;
+      if(randomPlayer<0.5)
+        this.setState({playerTwo:players[1]});
+      else
+        this.setState({playerOne:players[1]});
+    }
+    else
+    {
+      if(randomPlayer<0.5)
+        this.setState({playerOne:players[0]});
+      else
+        this.setState({playerTwo:players[0]});
+    }
+  }
+  restart = () => {
+    this.setState({
+      playerOne: {currentScore: 0, newScore:0, name: "Roger", img:"/img/roger.png"},
+      playerTwo: {currentScore: 0, newScore:0, name: "Tim", img:"/img/tim.png"}
+    });
+  }
+  render = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-3">
+      <Jumbotron>
+        <h1 className="header">Tennis Scoreboard</h1>
+        <Row>
+          <Col sm>
+            <Player align="left" data={this.state.playerOne}></Player>
+          </Col>
+          <Col sm>
+            <div className="score-button">
+              <Button onClick={this.addScore}>Random</Button>
+            </div>
+          </Col>
+          <Col sm>  
+            <Player align="right" data={this.state.playerTwo}></Player>
+          </Col>
+        </Row>
+      </Jumbotron>
+    </Container>
   );
+  }
 }
 
 export default App;
---
+++ src/components/Player.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+
+import './Player.css'
+
+const Player = (props) => {
+
+    let scores = ["0","15","30","40","A"];
+    console.log(123);
+    console.log(props);
+    return (
+    <Card style={{ width: 'calc(3rem + 14vmin)' }} className={props.align}>
+        <Card.Header>
+            <Image src={props.data.img} roundedCircle className="avatar"/>
+            <Card.Title>{props.data.name}</Card.Title>
+        </Card.Header>
+        <Card.Body>
+            <div className="score-card">
+                <div className="score front">{scores[props.data.currentScore]}</div>
+                <div className="score back">{scores[props.data.newScore]}</div>
+            </div>
+        </Card.Body>
+    </Card>
+    );
+}
+
+export default Player;
