e546647e6497389ffa337f864249e67267eda29c
Tomasz <krzykophil@gmail.com>
2020-05-31 01:51:26 +0100

    Add Initial socket handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 
+import socketIOClient from "socket.io-client";
+
+
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -14,12 +17,28 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.ENDPOINT = "http://127.0.0.1:4000";
     this.state = {
+      msg: "Init",
       gameFinished: false,
       winner: {},
       playerOne: {currentScore: 0, newScore:0, name: "Roger", img:"/img/roger.png"},
       playerTwo: {currentScore: 0, newScore:0, name: "Tim", img:"/img/tim.png"}
     };
+  }
+  componentDidMount = () => {
+    const socket = socketIOClient(this.ENDPOINT);
+    socket.on("Bump", data => {
+      this.setState({msg: data});
+    });
+    socket.on("abandoned", data => {
+      //TODO show button to join new game
+      this.setState({msg: data.opponent+" left game"});
+    });
+    socket.on("move state", data => {
+      //TODO update scores and display message
+    });
+    socket.emit("new player",Math.random());
   }
   toggleShow = () => {
     this.setState({gameFinished:false})
@@ -64,6 +83,7 @@ class App extends Component {
     <Container className="p-3">
       <Jumbotron>
         <h1 className="header">Tennis Scoreboard</h1>
+        <div>{this.state.msg}</div>
         <div style={{position:"relative"}}>
           <Overlay show={this.state.gameFinished} winner={this.state.winner} toggleShow={this.toggleShow.bind(this)} />
           <Row>
